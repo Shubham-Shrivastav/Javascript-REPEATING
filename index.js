f5b3ec445fdc48f890bfe5afba2e3019492b6ebd
Shubham-Shrivastav <shuboom@969@gmail.com>
2023-03-28 12:11:37 +0530

    Initial commmit

---
+++ index.js
@@ -0,0 +1,160 @@
+//1.  Create a js file which contains variable declaration for a form which asks following basic details :
+//(What is your Name, Email, age, address, phone number, gender,
+//Are you married)
+
+// let name = "Shubham Shrivastav";
+// let Email = "shubham@gmail.com";
+// let age = 25;
+// let address = "Indore";
+// let phoneNumber = "9876543456"
+// let gender = "Male";
+// let maritalStatus = false;
+
+//---------- 1. Declare all the variables in above form without initializing and print their value.
+
+// console.log("Name : " + name);
+// console.log("Email : " + Email);
+// console.log("age : " + age);
+// console.log("address : " + address);
+// console.log("phoneNumber : " + phoneNumber);
+// console.log("gender : " + gender);
+// console.log("maritalStatus : " + maritalStatus);
+
+//------- 2. After printing their value assign them as null and print it again.
+
+// let name = null;
+// let Email = null;
+// let age = null;
+// let address = null;
+// let phoneNumber = null;
+// let gender = null;
+// let maritalStatus = null;
+
+// console.log("Name : " + name);
+// console.log("Email : " + Email);
+// console.log("age : " + age);
+// console.log("address : " + address);
+// console.log("phoneNumber : " + phoneNumber);
+// console.log("gender : " + gender);
+// console.log("maritalStatus : " + maritalStatus);
+
+// ------  3. Declare an object called employeeDetails and add all the above variables to this object now as keys with value as null and print this object.
+
+// let employeeDetails = {
+//     name: name,
+//     email: Email,
+//     age: age,
+//     address: address,
+//     phoneNumber: phoneNumber,
+//     gender: gender,
+//     maritalStatus: maritalStatus
+//  }
+// console.log(employeeDetails);
+
+// -----1. In the variables declare above assign the variables as appropriate default value instead of null.
+
+// let name = "Shubham Shrivastav";
+// let Email = "shubham@gmail.com";
+// let age = 25;
+// let address = "Indore";
+// let phoneNumber = "9876543456"
+// let gender = "Male";
+// let maritalStatus = false;
+
+// let employeeDetails = {
+//     name: name,
+//     email: Email,
+//     age: age,
+//     address: address,
+//     phoneNumber: phoneNumber,
+//     gender: gender,
+//     maritalStatus: maritalStatus
+//  }
+// console.log(employeeDetails);
+
+// ---Take two numbers and perform all the arithmetic operations over them.
+
+
+// let a;
+// let b;
+// let c;
+// let w;
+// let x;
+// let y;
+// let z;
+// a = 3;
+// b = 8;
+// c = a + b;
+// w = a - b;
+// x = a * b;
+// y = a / b;
+// z = a % b;
+
+// let Ans = {
+//     "a + b = " : c,
+//     "a - b = " : w,
+//     "a * b = ": x,
+//     "a / b = ": y,
+//     "a % b = ": z
+// }
+// console.log(Ans);
+
+
+// --- Q2. Take 2 numbers and 2 strings and perform addition operation on them in same sequence.
+
+// let a;
+// let b;
+// let c;
+// let w;
+// let x;
+// let y;
+// let z;
+// a = 10;
+// b = "dk";
+// c = a + b;
+// w = a - b;
+// x = a * b;
+// y = a / b;
+// z = a % b;
+
+// let Ans = {
+//     "a + b = " : c,
+//     "a - b = " : w,
+//     "a * b = ": x,
+//     "a / b = ": y,
+//     "a % b = ": z
+// }
+// console.log(Ans);
+
+// Take 2 Strings and 2 numbers and perform addition operation on them in same sequence.
+
+// console.log("Jk" + 12);
+// console.log("Jk" - 12);
+// console.log("Jk" / 12);
+// console.log("Jk" * 12);
+// console.log("Jk" % 12);
+
+// --- 4. Write an operation to get its result as NaN.
+
+// console.log("JK" * "Ik12");
+
+
+// --- Q5. Take 2 boolean variables as true and false and perform And and Or logical operation over them and print the
+//result as well as operation.
+
+// let a = true;
+// let b = false;
+// console.log(a && b);
+// console.log(a || b);
+// console.log(a != b);
+
+//--- Q6 Take 2 variables and compare them using == and
+// === such that result of both the comparisons operation
+// is not equal."
+
+// let a = "10";
+// let b = 10;
+
+// console.log(a == b);
+// console.log(a === b);
+
